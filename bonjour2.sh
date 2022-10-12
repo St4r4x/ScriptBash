318a522d09b256aac3ac54a042e25e3737983ba9
St4r4x <st4r4x@gmail.com>
2022-10-12 15:30:41 +0200

    q

---
+++ bonjour2.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+echo "Bonjour tout le monde"
+echo "2 fois Bonjour tout le monde"
+echo le \#\ est populaire
+echo le \#\ est plus populaire que le \\\
+echo le \#\ est plus populaire que le \\\ mais moins bien que l\'*\\
+echo le '#' est moins populaire que le '*'
+echo "le # est moins populaire que le *"
+echo le '#' est plus populaire que le '\'
+echo "le # est plus populaire que le \ "
---
+++ script.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+# echo "bonjour les devs !"
+# # déclaration d'une variable bash
+# x=5
+# echo "x=$x"
+# read y
+# echo "y=$y"
+# # structure conditionnelle
+# if [ $y -eq $x ]; then
+#     echo "$x est égal à $y"
+# elif [ $x -lt $y ]; then
+#     echo "$x < $y"
+# else
+#     echo "$x > $y"
+# fi
+# manipulation des arguments d'un script
+echo "$1"
+echo "$2"
+echo "$3"
+
